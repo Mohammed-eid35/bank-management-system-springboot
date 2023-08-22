ebe23950da06bfeedd54702d3b4ef541ed2e7e66
Muhammad Eid <meid95874@gmail.com>
2023-08-22 16:35:16 +0300

    Unit Testing for UserRepository

--- src/test/java/com/alien/bank/management/system/repository/UserRepositoryTest.java
+++ src/test/java/com/alien/bank/management/system/repository/UserRepositoryTest.java
@@ -5,7 +5,7 @@ import com.alien.bank.management.system.entity.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class UserRepositoryTest {
 
-    @Autowired
+    @InjectMocks
     private UserRepository userRepository;
 
     private User user;
